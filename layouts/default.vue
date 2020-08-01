b4fcb6772ecada0b651acb41e52bc4a29f78dfcb
gatchan0807 <yuki.dolphin.koga@gmail.com>
2020-08-01 11:08:37 +0900

    ベースのスタイルアテ

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
+  <div class="container">
     <Nuxt />
   </div>
 </template>
 
-<style>
+<style lang="postcss">
+html,
+body,
+.container {
+  @apply h-screen;
+}
+
+body {
+  margin: 0;
+  overflow: hidden;
+  color: #f4f4f4;
+}
+
+.container {
+  @apply mx-auto;
+}
 </style>
